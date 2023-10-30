8b8a3d5fbf40c6a16f9c5fe1cc8152ec29601663
hoangvuonq <hoanvuonq2002@gmail.com>
2023-10-31 00:20:55 +0700

    Comment Avatar

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="bg-white w-full h-full">  
             <div className="w-full h-full">
                 <div className="all-center w-full pt-[100px] flex-col">
-                    <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] sm:h-[400px] h-[300px] rounded-full border-[6px] border-[#747474]" />
+                    {/* <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] sm:h-[400px] h-[300px] rounded-full border-[6px] border-[#747474]" /> */}
                     <h1 className="text-title text-black text-s sm:!text-[100px] !text-[80px] with-shadow">Nothing</h1>
                     <p className="text-content text-black text-s text-center sm:px-0 px-2">CA :</p>
                     <p className="text-content text-black text-s text-center sm:px-0 px-2"></p>
