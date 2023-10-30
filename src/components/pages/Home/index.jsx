bd9fb26768cc0eb4bd89b79eef971c3ded324bc9
hoangvuonq <hoanvuonq2002@gmail.com>
2023-10-31 00:12:35 +0700

    Update UI Nothing

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../../assets/avatar.jpg" 
-import Tokenmics from "../Tokenmics";
+import Avatar from "../../../assets/avatar.png" 
 import Meet from "../Meet";
 import Footer from "../../layouts/Footer";
 
 const Home = () => {
     return (
-        <div className="bg-kog w-full h-full">  
-            <div className="bg-custom w-full h-full">
+        <div className="bg-white w-full h-full">  
+            <div className="w-full h-full">
                 <div className="all-center w-full pt-[100px] flex-col">
-                    <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] rounded-full border-[6px] border-[#FDF436]" />
-                    <h1 className="text-title text-white text-s sm:!text-[100px] !text-[80px] with-shadow">PEPIE</h1>
-                    <p className="text-content text-white text-s text-center sm:px-0 px-2">CA :</p>
-                    <p className="text-content text-white text-s text-center sm:px-0 px-2">PEPIE THE ZOMBIE</p>
+                    <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] sm:h-[400px] h-[300px] rounded-full border-[6px] border-[#747474]" />
+                    <h1 className="text-title text-black text-s sm:!text-[100px] !text-[80px] with-shadow">Nothing</h1>
+                    <p className="text-content text-black text-s text-center sm:px-0 px-2">CA :</p>
+                    <p className="text-content text-black text-s text-center sm:px-0 px-2"></p>
                     <div className="all-center gap-10 mt-10 lg:flex-row flex-col">
                         <Link to={"#"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
+                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
                         </Link>
-                        <Link to={"https://t.me/PepeTheWorldEatter"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
+                        <Link to={"https://t.me/erc20_nothing"} target="_blank">
+                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
                         </Link>
-                        <Link to={"https://twitter.com/PepeWorldEatter"} target="_blank">
-                            <button className="bg-white text-[#432627] py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
+                        <Link to={"https://twitter.com/nothingERC20_"} target="_blank">
+                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
                         </Link>
                     </div>
                     <Meet/>
-                    <Tokenmics/>
                 </div>
                 <Footer/>
             </div>
--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 const Meet = () => {
     return (
-        <div className="rounded-[40px] border-8 border-white shadow-lg backdrop-blur-md mt-32 w-[80%] p-8">
+        <div className="rounded-[40px] border-8 border-black shadow-custom-meet blur-10 mt-32 w-[80%] p-8">
             <div className="flex-col all-center sm:p-20 p-8">
-                <h1 className="text-title-box text-white text-center">MEET $PEPIE</h1>
-                <p className="text-content-box text-white pt-10">
-                    Pepie, also known as Pepe the Zombie, is a unique and enigmatic character in the digital world. 
-                    Unlike the typical Pepe meme, Pepie stands out with his eerie appearance. 
-                    He is portrayed as a zombie-like creature with pallid, decaying green skin and vacant, haunting eyes. 
-                    Pepie's presence evokes a sense of unease and curiosity, making him a distinct and memorable figure in the meme universe.
+                <h1 className="text-title-box text-black text-center">MEET $Nothing</h1>
+                <p className="text-content-box text-black pt-10 text-center sm:text-[120px] text-[80px]">
+                    nothing here
                 </p>
             </div>
         </div>
