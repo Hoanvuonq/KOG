706675fa685813e43bd79e759197308e97105b4f
hoangvuonq <hoanvuonq2002@gmail.com>
2023-11-01 14:26:52 +0700

    UI page Bad Luck Brian , Build

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../../assets/avatar.png" 
+// import Avatar from "../../../assets/avatar.png" 
 import Meet from "../Meet";
 import Footer from "../../layouts/Footer";
 
 const Home = () => {
     return (
-        <div className="bg-white w-full h-full">  
+        <div className="bg-custom w-full h-full">  
             <div className="w-full h-full">
                 <div className="all-center w-full pt-[100px] flex-col">
                     {/* <img src={Avatar} alt="Item EOG" className="sm:w-[400px] w-[300px] sm:h-[400px] h-[300px] rounded-full border-[6px] border-[#747474]" /> */}
-                    <h1 className="text-title text-black text-s sm:!text-[100px] !text-[80px] with-shadow">Nothing</h1>
-                    <p className="text-content text-black text-s text-center sm:px-0 px-2">CA : 0x8be7e420988ac9c1d07f6ee653ccedf623ebaeae</p>
-                    <p className="text-content text-black text-s text-center sm:px-0 px-2"></p>
+                    <h1 className="text-title text-orange-600 text-s sm:!text-[100px] !text-[80px] with-shadow">Bad Luck Brian </h1>
+                    <p className="text-content text-orange-600 text-s text-center sm:px-0 px-2">CA : </p>
+                    <p className="text-content text-orange-600 text-s text-center sm:px-0 px-2"></p>
                     <div className="all-center gap-10 mt-10 lg:flex-row flex-col">
                         <Link to={"#"} target="_blank">
-                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
+                            <button className="bg-white text-orange-600 py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">dextools chart</button>
                         </Link>
-                        <Link to={"https://t.me/erc20_nothing"} target="_blank">
-                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
+                        <Link to={"#"} target="_blank">
+                            <button className="bg-white text-orange-600 py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">telegram</button>
                         </Link>
-                        <Link to={"https://twitter.com/nothingERC20_"} target="_blank">
-                            <button className="bg-black text-white py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
+                        <Link to={"#"} target="_blank">
+                            <button className="bg-white text-orange-600 py-5 px-10 rounded-xl w-[315px] h-[57px] shadow-custom text-btn all-center">twitter</button>
                         </Link>
                     </div>
                     <Meet/>
--- src/components/pages/Meet/index.jsx
+++ src/components/pages/Meet/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Meet = () => {
     return (
-        <div className="rounded-[40px] border-8 border-black shadow-custom-meet blur-10 mt-32 w-[80%] p-8">
+        <div className="rounded-[40px] border-8 border-white shadow-custom-meet blur-10 mt-32 w-[80%] p-8">
             <div className="flex-col all-center sm:p-20 p-8">
-                <h1 className="text-title-box text-black text-center">MEET $Nothing</h1>
-                <p className="text-content-box text-black pt-10 text-center sm:text-[120px] text-[80px]">
-                    nothing here
+                <h1 className="text-title-box text-white text-center">Meet $BRIAN </h1>
+                <p className="text-content-box text-white pt-10 text-center ">
+                    Kyle Edward Craven (born August 10, 1989), commonly known by his Internet nickname 
+                    "Bad Luck Brian", is an American Internet celebrity known for his ubiquitous photo posted on Reddit in 2012, 
+                    which quickly became a popular Internet meme. Bad Luck Brian is an image macro style of meme. 
+                    His captions describe a variety of unlucky, embarrassing and tragic events.
                 </p>
             </div>
         </div>
