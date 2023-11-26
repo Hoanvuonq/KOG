55ce04d3f480161373da1059ee67e9bce889dfcf
hoanvuonq <hoanvuonq2002@gmail.com>
2023-11-26 23:27:07 +0700

    Update UI

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -8,11 +8,11 @@ import Footer from "../../layouts/Footer";
 const Home = () => {
     return (
         <div className="bg-kog w-full h-full">  
-            <div className="bg-[#01010178] w-full h-full">
+            <div className="bg-[#01010178] w-full h-full flex flex-col justify-between">
                 <div className="all-center w-full pt-[20px] flex-col">
                     <img src={Avatar} alt="Item EOG" className="sm:w-[150px] w-[200px] rounded-full border-[6px] border-[#FDF436]" />
                     <h1 className="text-title text-white text-s sm:!text-[30px] !text-[80px] sm:leading-[50px] with-shadow">$AI</h1>
-                    <p className="text-content text-white text-center">CA :</p>
+                    <p className="text-content text-white text-center">CA : 0xe62BB48bbB7334b3EADf7F70b3F3E65c776F6507</p>
                     {/* <p className="text-content text-white text-center">AI The Squid Game</p> */}
                     {/* <div className="all-center gap-10 mt-4 lg:flex-row flex-col">
                         <Link to={"#"} target="_blank">
