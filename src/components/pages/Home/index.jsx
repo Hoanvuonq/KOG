9beeaf22abc4194669073d5da36dd9cea4ea2e1d
kismynamefr <kismyname.fr@gmail.com>
2023-11-28 00:44:04 +0700

    push CA

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                 <div className="all-center w-full pt-[20px] flex-col">
                     <img src={Avatar} alt="Item EOG" className="sm:w-[150px] w-[200px] border-[4px] border-[#52ccc2]" />
                     <h1 className="text-title text-white text-s sm:!text-[30px] !text-[80px] sm:leading-[50px] with-shadow">$BITAPAI</h1>
-                    <p className="text-content text-white text-center sm:!text-base !text-xl">CA : </p>
+                    <p className="text-content text-white text-center sm:!text-base !text-xl max-w[80%] overflow-hidden text-ellipsis">CA : 0xc7A320CA7606770ce6360cec380435057C687065</p>
                     {/* <p className="text-content text-white text-center">AI The Squid Game</p> */}
                     {/* <div className="all-center gap-10 mt-4 lg:flex-row flex-col">
                         <Link to={"#"} target="_blank">
